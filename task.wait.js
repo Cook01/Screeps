f157fa3e159ef69241261268c787878cba77a6f1
Vincent THOMAS <vincentthomasetude@gmail.com>
2025-06-07 11:32:43 +0200

    Update task.wait.js

--- task.wait.js
+++ task.wait.js
@@ -3,7 +3,7 @@ module.exports = {
         // Move to a flag or idle spot if defined, otherwise stay still
         const flag = Game.flags['IdleZone'];
         if (flag) {
-            creep.moveTo(flag, { visualizePathStyle: { stroke: '#cccccc' } });
+            creep.moveTo(flag, { visualizePathStyle: { stroke: '#2F4F4F' } });
         }
     }
 };
